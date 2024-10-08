e380ac4863b0c2261345829ba5ae3d9ea3e1a0ee
Hebun <selmancevik21@gmail.com>
2024-10-08 21:08:41 +0300

    Initial commit

---
+++ app/src/main/java/com/github/translation/Translation.java
@@ -0,0 +1,12 @@
+package com.github.translation;
+
+import android.content.Context;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Translation {
+}
